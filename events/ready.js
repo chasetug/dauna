365732089a3d830abf441c09216faae3049ce8eb
Chase Tuggle <chasetug@gmail.com>
2021-06-12 20:56:08 -0500

    update

--- events/ready.js
+++ events/ready.js
@@ -3,10 +3,11 @@ module.exports = {
 	once: true,
 	async execute(client) {
 		const guildID = '782438773690597389';
-		console.log('Ready!');
+		console.log('Logged in and ready!');
 
 		const guild = client.guilds.cache.get(guildID);
 		await guild.members.fetch();
+		console.log('Fetched member cache!');
 
 		client.user.setActivity('GTFO', { type: 'PLAYING' })
 			.then(presence => console.log(`Activity set to ${presence.activities[0].name}`))
