51b96d125e7479029c4a5a7aa4ee4a8b23aed294
Chase Tuggle <chasetug@gmail.com>
2021-07-24 15:34:27 -0500

    fixed if command does not exist

--- src/commands/help.js
+++ src/commands/help.js
@@ -4,6 +4,7 @@ module.exports = {
 	async execute(client, message, args) {
 		// const msg = await message.channel.send('Ping?');
 		const command = client.commands.get(args[0]);
+		if (!command) return message.channel.send('That command does not exist');
 		message.channel.send(`Command: ${command.name}\nDescription: ${command.description}`);
 	},
 };
