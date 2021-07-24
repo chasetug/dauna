c725330aed366e4228a3332b067ec5fdb7db9fb8
Chase Tuggle <chasetug@gmail.com>
2021-07-24 15:33:14 -0500

    disabled for now

--- src/events/presenceUpdate.js
+++ src/events/presenceUpdate.js
@@ -2,15 +2,15 @@ module.exports = {
 	name: 'presenceUpdate',
 	execute(oldPresence, newPresence, client) {
 		const { member, activities } = newPresence;
-		const streamingActivity = activities.find(a => a.type == 'STREAMING');
+		const streamingActivity = activities.find(a => a.type === 'STREAMING');
 
 		if (!streamingActivity) return;
 		const { state, details, url } = streamingActivity;
 
-		if (state != 'GTFO') return;
+		if (state !== 'GTFO') return;
 		if (!details.toLowerCase().includes('mod')) return;
 
 		const channel = client.channels.cache.get('787788643983949844');
-		channel.send(`**${member.nickname}** is currently streaming GTFO!\nCheck out **${details}** here:\n${url}`);
+		// channel.send(`**${member.user.username}** is currently streaming GTFO!\nCheck out **${details}** here:\n${url}`);
 	},
 };
