9d9bfade0a191ee58171b084921021c9aaa0ce25
Chase Tuggle <chasetug@gmail.com>
2021-07-24 15:32:40 -0500

    add reactions to suggestions

--- src/commands/suggestion.js
+++ src/commands/suggestion.js
@@ -4,15 +4,19 @@ module.exports = {
 	name: 'suggestion',
 	description: 'Posts a suggestion into the suggestion channel',
 	execute(client, message, args) {
-		if (!args) return message.channel.send('Please specify a suggestion!');
+		if (!args[0]) return message.channel.send('Please include a suggestion! Usage: !suggestion < suggestion >');
 		const channel = client.channels.cache.get('782451443311050793');
 
 		const embed = new MessageEmbed()
 			.setColor('#4e704e')
-			.setAuthor(message.member.nickname, message.member.user.avatarURL())
+			.setAuthor(message.member.user.username, message.member.user.avatarURL())
 			.addField('Suggestion', args.join(' '))
 			.setTimestamp()
 			.setFooter('!suggestion');
-		channel.send(embed).catch(console.error);
+		const suggestion = channel.send(embed).then(m => {
+			m.react('⬆').catch(console.error);
+			m.react('⬇').catch(console.error);
+		});
+		message.channel.send('Your suggestion has been posted.');
 	},
 };
