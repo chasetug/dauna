dd9853330198b2d42abf6cd4a47ef1a120db523c
Chase Tuggle <chasetug@gmail.com>
2021-10-13 14:18:00 -0500

    force string type

--- src/events/guildMemberAdd.js
+++ src/events/guildMemberAdd.js
@@ -16,8 +16,8 @@ module.exports = {
 			.setAuthor(`${member.user.username} (${member.id})`, member.user.avatarURL() == null ? 'https://images-ext-2.discordapp.net/external/6tVaUxectogf8lZc5X8fWTGd2tbzlG6I5AtVbWYYLNI/https/cdn.discordapp.com/embed/avatars/4.png' : member.user.avatarURL())
 			.setThumbnail(member.user.avatarURL() == null ? 'https://images-ext-2.discordapp.net/external/6tVaUxectogf8lZc5X8fWTGd2tbzlG6I5AtVbWYYLNI/https/cdn.discordapp.com/embed/avatars/4.png' : member.user.avatarURL())
 			.addFields(
-				{ name: 'Joined Guild', value: member.joinedAt, inline: true },
-				{ name: 'Joined Discord', value: member.user.createdAt, inline: true },
+				{ name: 'Joined Guild', value: `${member.joinedAt}`, inline: true },
+				{ name: 'Joined Discord', value: `${member.user.createdAt}`, inline: true },
 			)
 			.setTimestamp()
 			.setFooter('Member joined');
